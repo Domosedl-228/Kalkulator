eaaa012b935c2038fe01abde9b61875c683775a6
Acer <Acer@DESKTOP-Q4A94PR>
2023-09-08 14:38:02 +0700

    Добавьте файлы проекта.

---
+++ Kalkulator/Program.cs
@@ -0,0 +1,25 @@
+﻿
+
+class Calculator
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите выражение:");
+        string expression = Console.ReadLine();
+
+        try
+        {
+            double result = EvaluateExpression(expression);
+            Console.WriteLine("Результат: " + result);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Ошибка: " + ex.Message);
+        }
+    }
+
+    static double EvaluateExpression(string expression)
+    {
+        return Convert.ToDouble(new System.Data.DataTable().Compute(expression, null));
+    }
+}
